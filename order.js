d7c4f11214bf892085a2488d8cdfe804b894807f
amaliegrøn <amal810c@stud.kea.dk>
2021-05-19 20:17:52 +0200

    skelet opsat, billedegalleri og orderliste virker

---
+++ order.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", init);
+
+// let json;
+let url = "https://teamellewoods.herokuapp.com/";
+
+async function init() {
+  setTimeout(refreshDashboard /* 1000 */);
+}
+
+async function refreshDashboard() {
+  const respons = await fetch(url);
+  const data = await respons.json();
+
+  //update being severd
+  document.querySelector("#serving_list").innerHTML = "";
+  data.serving.forEach((customer) => {
+    const element = getCustomerElement(customer);
+    document.querySelector("#serving_list").appendChild(element);
+  });
+
+  /* setTimeout(refreshDashboard, 1000); */
+}
+
+function getCustomerElement(customer) {
+  const element = document.querySelector("template#customer").content.cloneNode(true);
+  element.querySelector(".id").textContent = customer.id;
+  return element;
+}
